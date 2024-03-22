c72eb4095ee41b45243b6385dc8962b641bff59f
Lőkös Patrik <67261375+ModzsoTheBeast@users.noreply.github.com>
2024-03-22 09:38:15 +0100

    translated

--- blog-front/src/app/home/sidenav/sidenav.component.ts
+++ blog-front/src/app/home/sidenav/sidenav.component.ts
@@ -12,10 +12,10 @@ import {NavButtonComponent} from "./nav-button/nav-button.component";
 })
 export class SidenavComponent {
   tags = [
-    { icon: 'assets/icons/boot.png', iconAlt: 'hiking-boot', name: 'Hiking', isChosen: true },
-    { icon: 'assets/icons/brain.png', iconAlt: 'brain', name: 'Psychology', isChosen: true },
-    { icon: 'assets/icons/cooking.png', iconAlt: 'cooking', name: 'Cooking', isChosen: true },
-    { icon: 'assets/icons/diary.png', iconAlt: 'diary', name: 'Journaling', isChosen: true },
-    { icon: 'assets/icons/shovels.png', iconAlt: 'shovels', name: 'Gardening', isChosen: true }
+    { icon: 'assets/icons/boot.png', iconAlt: 'hiking-boot', name: 'Utazás', isChosen: true },
+    { icon: 'assets/icons/brain.png', iconAlt: 'brain', name: 'Önismeret?', isChosen: true },
+    { icon: 'assets/icons/cooking.png', iconAlt: 'cooking', name: 'Étel-ital', isChosen: true },
+    { icon: 'assets/icons/diary.png', iconAlt: 'diary', name: 'Naplózás', isChosen: true },
+    { icon: 'assets/icons/shovels.png', iconAlt: 'shovels', name: 'Kert', isChosen: true }
   ];
 }
