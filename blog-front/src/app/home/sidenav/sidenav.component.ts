a9b672d0a88a8f20db10fb4e5e6ceae22b1b1986
Lőkös Patrik <67261375+ModzsoTheBeast@users.noreply.github.com>
2024-03-23 10:23:04 +0100

    resized sidenav

--- blog-front/src/app/home/sidenav/sidenav.component.ts
+++ blog-front/src/app/home/sidenav/sidenav.component.ts
@@ -29,29 +29,36 @@ import {BreakpointService} from "../../shared/services/breakpoint.service";
           width: '0'
         }
       )),
+      state('halfway', style(
+        {
+          width: '70px'
+        }
+      )),
       transition('open => closed', [animate('0.2s')]),
       transition('closed => open', [animate('0.5s')]),
+      transition('halfway => closed', [animate('0.2s')]),
+      transition('closed => halfway', [animate('0.2s')]),
     ])
   ]
 
 })
 export class SidenavComponent implements OnDestroy{
 
-  tags = [
+  tags:{icon: string, iconAlt: string, name: string, isChosen:boolean}[] = [
     { icon: 'assets/icons/boot.png', iconAlt: 'hiking-boot', name: 'Utazás', isChosen: true },
-    { icon: 'assets/icons/brain.png', iconAlt: 'brain', name: 'Önismeret?', isChosen: true },
-    { icon: 'assets/icons/cooking.png', iconAlt: 'cooking', name: 'Étel-ital', isChosen: true },
-    { icon: 'assets/icons/diary.png', iconAlt: 'diary', name: 'Naplózás', isChosen: true },
-    { icon: 'assets/icons/shovels.png', iconAlt: 'shovels', name: 'Kert', isChosen: true }
+    { icon: 'assets/icons/brain.png', iconAlt: 'brain', name: 'Önismeret?', isChosen: true},
+    { icon: 'assets/icons/cooking.png', iconAlt: 'cooking', name: 'Étel-ital', isChosen: true},
+    { icon: 'assets/icons/diary.png', iconAlt: 'diary', name: 'Naplózás', isChosen: true},
+    { icon: 'assets/icons/shovels.png', iconAlt: 'shovels', name: 'Kert', isChosen: true}
   ];
 
   currentBreakpoint:string = '';
   subscription?: Subscription;
 
   constructor(private breakpointService: BreakpointService) {
-    this.subscription= this.breakpointService.breakpoint$.subscribe(value => {
+    this.subscription = this.breakpointService.breakpoint$.subscribe(value => {
       this.breakpointService.breakpointChanged();
-      this.currentBreakpoint= this.breakpointService.currentBreakpoint.value;
+      this.currentBreakpoint = this.breakpointService.currentBreakpoint.value;
     });
   }
 
