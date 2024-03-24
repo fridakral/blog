a18ea7499275541b88b0c27fa8dffcdcfd36bc78
Lőkös Patrik <67261375+ModzsoTheBeast@users.noreply.github.com>
2024-03-24 12:13:03 +0100

    authors tag added

--- blog-front/src/app/home/home.component.ts
+++ blog-front/src/app/home/home.component.ts
@@ -31,13 +31,7 @@ export class HomeComponent implements OnInit, OnDestroy{
   isOpen: boolean = true;
   blogPostsToSend : {title: string, tags: string[], image: string}[] = [];
 
-  tags:{icon: string, iconAlt: string, name: string, isChosen:boolean}[] = [
-    { icon: 'assets/icons/boot.png', iconAlt: 'hiking-boot', name: 'Utazás', isChosen: true },
-    { icon: 'assets/icons/brain.png', iconAlt: 'brain', name: 'Önismeret?', isChosen: true},
-    { icon: 'assets/icons/cooking.png', iconAlt: 'cooking', name: 'Étel-ital', isChosen: true},
-    { icon: 'assets/icons/diary.png', iconAlt: 'diary', name: 'Naplózás', isChosen: true},
-    { icon: 'assets/icons/shovels.png', iconAlt: 'shovels', name: 'Kert', isChosen: true}
-  ];
+
 
   constructor(private breakpointService: BreakpointService) {
     this.subscription= this.breakpointService.breakpoint$.subscribe(value => {
---
+++ blog-front/src/app/shared/utils/consts.ts
@@ -0,0 +1,10 @@
+export const tags:{icon: string, iconAlt: string, name: string, isChosen:boolean}[] = [
+  { icon: 'assets/icons/boot.png', iconAlt: 'hiking-boot', name: 'Utazás', isChosen: true },
+  { icon: 'assets/icons/brain.png', iconAlt: 'brain', name: 'Önismeret?', isChosen: true},
+  { icon: 'assets/icons/cooking.png', iconAlt: 'cooking', name: 'Étel-ital', isChosen: true},
+  { icon: 'assets/icons/diary.png', iconAlt: 'diary', name: 'Naplózás', isChosen: true},
+  { icon: 'assets/icons/shovels.png', iconAlt: 'shovels', name: 'Kert', isChosen: true}
+];
+
+export const authors : string[] = ["Eme", "Bea", "Frida"];
+
